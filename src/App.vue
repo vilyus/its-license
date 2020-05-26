e4aa9130b16fed0ace6ace77e47b56b730902edb
Vilyus Bruzdeylinas <bvilyus@gmail.com>
2020-05-26 20:08:22 +0300

    license gen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <License />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import License from './components/License'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    License
   }
 }
 </script>
@@ -21,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
